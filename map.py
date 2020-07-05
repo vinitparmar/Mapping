bab6fcab8f3c8e116f3972583d8da37b53897b2e
vinit parmar <vinitparmar89@gmail.com>
2020-07-06 00:39:16 +0530

    [implement]- color based on population

--- map.py
+++ map.py
@@ -20,5 +20,9 @@ def color_producer(ele):
 for lt, ln, el in zip(lat, lon, elev):
     fg.add_child(folium.CircleMarker(location=[lt,ln],popup=str(el)+" m",color='grey',radius =6,fill_color=color_producer(el),fill_opacity=0.7))
 
+
+fg.add_child(folium.GeoJson(data=open('datafiles/world.json','r',encoding='utf-8-sig').read(),style_function = lambda x: {'fillColor':'green' if x['properties']['POP2005'] < 10000000 
+else 'orange' if 10000000 <= x['properties']['POP2005'] < 20000000 else 'red'}))
+
 map.add_child(fg)
 map.save("Map.html")
