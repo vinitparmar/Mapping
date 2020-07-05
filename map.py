a3206ef8126a1812b0dfd43ed60730d2454fb5ab
vinit parmar <vinitparmar89@gmail.com>
2020-07-05 20:26:26 +0530

    [implement]- marker on map

--- map.py
+++ map.py
@@ -1,5 +1,18 @@
 import folium
+import pandas
 
-map = folium.Map(location=[28.70, 77.10],zoom_start=6,titles="Stamen Terrain")
+# reading the data form the file
+data =  pandas.read_csv("datafiles/Volcanoes.txt")
+lat =  list(data["LAT"])
+lon =  list(data["LON"])
+# lands on the lat long location on load of the page 
+map = folium.Map(location=[38.58, -99.09],zoom_start=6,tiles="OpenStreetMap")
+# adds the marker at the location with pop up
+fg = folium.FeatureGroup(name="My Map")
 
+# for coordinates in [[28.40,77.58],[28.80,77.98]]:
+for lt, ln in zip(lat, lon):
+    fg.add_child(folium.Marker(location=[lt,ln],popup="hi ..i am a marker",icon=folium.Icon(color="green")))
+
+map.add_child(fg)
 map.save("Map.html")
