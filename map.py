b4d49618452ac87a009153945053641f7c5b1da2
vinit parmar <vinitparmar89@gmail.com>
2020-07-05 23:25:49 +0530

    [implement]- convert marker to dot marker

--- map.py
+++ map.py
@@ -5,14 +5,20 @@ import pandas
 data =  pandas.read_csv("datafiles/Volcanoes.txt")
 lat =  list(data["LAT"])
 lon =  list(data["LON"])
+elev = list(data["ELEV"])
 # lands on the lat long location on load of the page 
 map = folium.Map(location=[38.58, -99.09],zoom_start=6,tiles="OpenStreetMap")
 # adds the marker at the location with pop up
 fg = folium.FeatureGroup(name="My Map")
 
-# for coordinates in [[28.40,77.58],[28.80,77.98]]:
-for lt, ln in zip(lat, lon):
-    fg.add_child(folium.Marker(location=[lt,ln],popup="hi ..i am a marker",icon=folium.Icon(color="green")))
+# color producer function
+def color_producer(ele):
+    if(ele <= 1000): return 'green'
+    if(ele > 1000 and ele <= 3000): return 'orange'
+    if(ele > 3000): return 'red'
+
+for lt, ln, el in zip(lat, lon, elev):
+    fg.add_child(folium.CircleMarker(location=[lt,ln],popup=str(el)+" m",color='grey',radius =6,fill_color=color_producer(el),fill_opacity=0.7))
 
 map.add_child(fg)
 map.save("Map.html")
